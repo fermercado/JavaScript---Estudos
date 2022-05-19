9b1a4eaa490355e203f775e249cecf7ba0ea5bce
Fernando Mercado <fermercado@live.com>
2022-05-19 16:37:11 -0300

    tipos de dados

---
+++ 2 - Tipos de dados/aulas copy.js
@@ -0,0 +1,19 @@
+var nome = 'Fernando';
+var idade = '34';
+var time = null;
+
+var nome = 'Fernando';
+var sobreNome = 'Mercado';
+var nomeCompleto = nome + ' ' + sobreNome;
+
+var gols = 1000;
+var frase = 'Romário fez ' + gols + ' gols';
+var frase2 = `Romario fez ${gols} gols`;
+console.log(frase2);
+
+var ano = '2018';
+var mes = 8;
+console.log(ano + mes);
+
+var frase1 = 'esse é o "melhor" jogo';
+console.log(frase1);
---
+++ 2 - Tipos de dados/aulas.js
@@ -0,0 +1,12 @@
+var nome = 'Fernando';
+var sobreNome = 'Mercado';
+var ano = '2022';
+var idade = 34;
+
+var nomeCompleto = `${nome} ${sobreNome}`;
+
+var frase = "it's time";
+
+console.log(frase);
+
+console.log(nomeCompleto);

475d9e47d94b699fec5c01113fdf5778f11f207f
Fernando Mercado <fermercado@live.com>
2022-06-02 16:47:02 -0300

    exercicios numeros e operadores

--- 3 - Numeros e operadores 1/aulas.js
+++ 3 - Numeros e operadores 1/aulas.js
@@ -1,3 +1,24 @@
-var idade = 23;
+// Qual o resultado da seguinte expressão?
+var total = 10 + (5 * 2) / 2 + 20;
+console.log(total);
 
-console.log(idade);
+// Crie duas expressões que retornem NaN
+var expressao1 = 'teste' / 2;
+var expressao2 = 'teste' - 2;
+
+console.log(expressao1, expressao2);
+// Somar a string '200' com o número 50 e retornar 250
+var soma = +'200' + 50;
+console.log(soma);
+
+// Incremente o número 5 e retorne o seu valor incrementado
+var num = 5;
+console.log(++num);
+
+// Como dividir o peso por 2?
+var numero = +'80' / 2;
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+var pesoPorDois = peso; // NaN (Not a Number)
+
+console.log(pesoPorDois);

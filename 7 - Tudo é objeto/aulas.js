e51da1bb635205b9a5b4cd61089d54c4a066fe30
Fernando Mercado <fermercado@live.com>
2022-06-07 14:12:42 -0300

    exercicios objetos

---
+++ 7 - Tudo é objeto/aulas.js
@@ -0,0 +1,15 @@
+// nomeie 3 propriedades ou métodos de strings
+var nome = 'fernando';
+// fixed;
+// length;
+// slice;
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+
+let btn = document.querySelector('.btn');
+// btn.addEventListener
+// btn.appendChild
+// btn.after
+
+// busque na web um objeto (método) capaz de interagir com o clipboard,
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V

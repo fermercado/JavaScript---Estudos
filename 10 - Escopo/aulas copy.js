d0e44b81ef30c6000ef427d6cba3516c7e71916f
Fernando Mercado <fermercado@live.com>
2022-06-08 11:45:51 -0300

    exercicio escopo

---
+++ 10 - Escopo/aulas copy.js
@@ -0,0 +1,9 @@
+function mostrarCarro() {
+  var carro = 'Fusca';
+  console.log(carro);
+}
+
+mostrarCarro(); // Fusca no console
+console.log(carro); // Erro, carro is not defined
+
+for (let i = 0; i < 10; i++) {}

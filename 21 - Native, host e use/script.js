12bcd9be23da7f3ebab45e9f4a737b721c0c000c
Fernando Mercado <fermercado@live.com>
2022-06-16 15:31:26 -0300

    exercicio native hust user

---
+++ 21 - Native, host e use/script.js
@@ -0,0 +1,17 @@
+// Liste 5 objetos nativos
+Object;
+String;
+Array;
+Function;
+Math;
+Date;
+
+// Liste 5 objetos do browser
+Window;
+Document;
+HTMLAllCollection;
+NodeList;
+history;
+
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox

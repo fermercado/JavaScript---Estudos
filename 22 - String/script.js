70c2492eb03763c269da84ab963a747c1615a645
Fernando Mercado <fermercado@live.com>
2022-06-18 16:23:46 -0300

    upgrade js string

--- 22 - String/script.js
+++ 22 - String/script.js
@@ -15,9 +15,49 @@ const listaFrutas = 'Melancia, Banana, Laranja';
 console.log(listaFrutas.includes(fruta)); // true
 console.log(fruta.includes(listaFrutas)); // false
 
-listaFrutas.forEach((x) => {
-  const nova = fruta.find((y) => x === y);
-  if (nova) {
-    console.log(nova);
-  }
+fruta.endsWith('nana'); // true
+fruta.startsWith('Ba'); // true
+fruta.startsWith('na'); // false
+
+const transacao1 = 'Depósito de cliente';
+const transacao2 = 'Depósito de fornecedor';
+const transacao3 = 'Taxa de camisas';
+
+transacao1.slice(0, 3); // Dep
+transacao2.slice(0, 3); // Dep
+transacao3.slice(0, 3); // Tax
+
+transacao1.slice(12); // cliente
+transacao1.slice(-4); // ente
+transacao1.slice(3, 6); // ósi
+
+console.log(fruta.indexOf('B'));
+console.log(fruta.lastIndexOf('a'));
+
+const listaPrecos = ['R$ 99', 'R$ 199', 'R$ 12000'];
+
+listaPrecos.forEach((item) => {
+  console.log(item.padStart(10, '..'));
 });
+
+let listaItens = 'Camisas Bonés Calças Bermudas Vestidos Saias';
+listaItens = listaItens.replace(/[ ]+/g, ', ');
+
+console.log(listaItens);
+
+const arrayLista = listaItens.split(', ');
+console.log(arrayLista);
+
+const htmlText = '<div>O melhor item</div><div>A melhor lista</div>';
+
+const htmlArray = htmlText.split('div');
+const novoHtml = htmlArray.join('section');
+
+console.log(htmlText);
+console.log(htmlArray);
+console.log(novoHtml);
+
+const valor = '  R$ 23.00   ';
+valor.trim(); // 'R$ 23.00'
+valor.trimStart(); // 'R$ 23.00   '
+valor.trimEnd(); // '  R$ 23.00'

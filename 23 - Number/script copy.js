24b6620c50422479750da3e92c95b4f4e73ba63e
Fernando Mercado <fermercado@live.com>
2022-06-19 23:25:19 -0300

    exercicios number

---
+++ 23 - Number/script copy.js
@@ -0,0 +1,11 @@
+let preco = 500;
+
+preco = preco.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+console.log(preco);
+
+console.log(Math.PI);
+
+const aleatorio = Math.floor(Math.random() * (40 - 20 + 1) + 20);
+
+console.log(aleatorio);
--- 23 - Number/script.js
+++ 23 - Number/script.js
@@ -1,11 +1,35 @@
-let preco = 500;
+// Retorne um número aleatório
+// entre 1050 e 2000
 
-preco = preco.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-
-console.log(preco);
-
-console.log(Math.PI);
-
-const aleatorio = Math.floor(Math.random() * (40 - 20 + 1) + 20);
+const aleatorio = Math.floor(Math.random() * (2000 - 1050 + 1) + 1050);
 
 console.log(aleatorio);
+
+// Retorne o maior número da lista abaixo
+let numeros = '4, 5, 20, 8, 9';
+let newNum = numeros.split(', ');
+const numeroMaximo = Math.max(...newNum);
+
+console.log(numeroMaximo);
+
+// Crie uma função para limpar os preços
+// e retornar os números com centavos arredondados
+// depois retorne a soma total
+const listaPrecos = ['R$ 59,99', ' R$ 100,222', 'R$ 230  ', 'r$  200'];
+
+let soma = 0;
+
+listaPrecos.forEach((item) => {
+  let newList = +item
+    .toLocaleUpperCase()
+    .replace('R$', '')
+    .trim()
+    .replace(',', '.');
+
+  newList = +newList.toFixed(2);
+  soma = soma + newList;
+});
+
+console.log(
+  soma.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }),
+);

e5c4e79360cad2d95f491a92da3cda9bae04581d
Fernando Mercado <fermercado@live.com>
2022-06-21 14:16:42 -0300

    exercicios array interações

--- 24 - Array interações/script.js
+++ 24 - Array interações/script.js
@@ -1,132 +1,76 @@
-// const carros = ['Ford', 'Fiat', 'Honda'];
-// carros.forEach(function (item, index, array) {
-//   console.log(item.toUpperCase());
-// });
+// Selecione cada curso e retorne uma array
+// com objetos contendo o título, descricao,
+// aulas e horas de cada curso
 
-// // com Arrow Function
-// carros.forEach((item, index, array) => {
-//   console.log(item.toUpperCase());
-// });
+const curso = document.querySelectorAll('.curso');
+const arrayCurso = Array.from(curso);
 
-// const li = document.querySelectorAll('li');
-
-// li.forEach((i) => i.classList.add('ativa'));
-
-// li.forEach(function (item) {
-//   item.classList.add('ativa');
-// });
-
-// const carros = ['Ford', 'Fiat', 'Honda'];
-// const newCarros = carros.map((item) => {
-//   return 'Carro ' + item;
-// });
-
-// carros; // ['Ford', 'Fiat', 'Honda']
-// newCarros; // ['Carro Ford', 'Carro Fiat', 'Carro Honda'];
-
-// const numeros = [2, 4, 6, 8, 10, 12, 14];
-// const numerosX3 = numeros.map((n) => n * 3);
-
-// numerosX3; // [6, 12, 18, 24, 30, 36, 42];
-
-// const aulas = [
-//   {
-//     nome: 'HTML 1',
-//     min: 15,
-//   },
-//   {
-//     nome: 'HTML 2',
-//     min: 10,
-//   },
-//   {
-//     nome: 'CSS 1',
-//     min: 20,
-//   },
-//   {
-//     nome: 'JS 1',
-//     min: 25,
-//   },
-// ];
-// let soma = 0;
-// const tempoAula = aulas.map((aula) => aula.min);
-
-// function nomeAulas(aula) {
-//   return aula.nome;
-// }
-
-// const arrayAula = aulas.map(nomeAulas);
-// console.log(arrayAula);
-
-// console.log(tempoAula);
-
-const aulas = [10, 25, 30];
-const total1 = aulas.reduce((acumulador, atual) => {
-  return acumulador + atual;
-}, 0);
-console.log(total1);
-
-const total2 = aulas.reduce((acc, cur) => acc + cur, 100);
-total2; // 165
-
-const frutas = ['Banana', 'Perâ', 'Uva'];
-
-const temUva = frutas.some((item) => {
-  return item === 'Uva';
+const newArray = arrayCurso.map((curso) => {
+  const titulo = curso.querySelector('h1').innerText;
+  const descr = curso.querySelector('p').innerText;
+  const aulas = curso.querySelector('.aulas').innerText;
+  const horas = curso.querySelector('.horas').innerText;
+  return {
+    titulo: titulo,
+    descricao: descr,
+    aulas: aulas,
+    horas: horas,
+  };
 });
+console.log(newArray);
 
-const every = frutas.every((fruta) => {
-  return fruta;
-});
+// Retorne uma lista com os
+// números maiores que 100
+const numeros = [3, 44, 333, 23, 122, 322, 33];
+const newNum = numeros.filter((item) => item > 100);
+console.log(newNum);
 
-console.log(every);
+// Verifique se Baixo faz parte
+// da lista de instrumentos e retorne true
+const instrumentos = ['Guitarra', 'Baixo', 'Bateria', 'Teclado'];
 
-const numeros = [6, 43, 22, 88, 101, 29];
-const maiorQue3 = numeros.every((x) => x > 3); // true
+const newInstrumento = instrumentos.some(
+  (instrumento) => instrumento === 'Baixo',
+);
 
-console.log(maiorQue3);
+console.log(newInstrumento);
 
-const buscaUva = frutas.findIndex((fruta) => {
-  return fruta === 'Uva';
-}); // 2
-
-console.log(buscaUva);
-
-const buscaMaior45 = numeros.find((x) => x > 45); // 88
-
-console.log(buscaMaior45);
-
-const frutas2 = ['Banana', undefined, null, '', 'Uva', 0, 'Maçã'];
-const arrayLimpa = frutas2.filter((fruta) => {
-  return fruta;
-}); // ['Banana', 'Uva', 'Maçã']
-
-console.log(arrayLimpa);
-
-const buscaMaior = numeros.filter((x) => x > 45); // [88, 101]
-
-console.log(buscaMaior);
-
-const aulas2 = [
+// Retorne o valor total das compras
+const compras = [
   {
-    nome: 'HTML 1',
-    min: 15,
+    item: 'Banana',
+    preco: 'R$ 4,99',
   },
   {
-    nome: 'HTML 2',
-    min: 10,
+    item: 'Ovo',
+    preco: 'R$ 2,99',
   },
   {
-    nome: 'CSS 1',
-    min: 20,
+    item: 'Carne',
+    preco: 'R$ 25,49',
   },
   {
-    nome: 'JS 1',
-    min: 25,
+    item: 'Refrigerante',
+    preco: 'R$ 5,35',
+  },
+  {
+    item: 'Quejo',
+    preco: 'R$ 10,60',
   },
 ];
 
-const maior15 = aulas2.filter((aula) => {
-  return aula.min >= 15;
-});
+const totalCompras = compras.reduce((acc, item) => {
+  const precoLimpo = +item.preco.replace('R$', '').replace(',', '.');
 
-console.log(maior15);
+  return acc + precoLimpo;
+}, 0);
+
+console.log(totalCompras);
+// let soma = 0;
+// const newCompras = compras.map((item) => {
+//   const newValue = +item.preco.replace('R$', '').replace(',', '.');
+
+//   soma += newValue;
+// });
+
+// console.log('total compras =>', soma);
